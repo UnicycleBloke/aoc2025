24d9680c2556f4a9123851808aa002bd54a13337
Al Chambers <unicycle.bloke@gmail.bloke>
2025-12-03 05:41:21 +0000

    Solved Day 3

--- day03/solution.cpp
+++ day03/solution.cpp
@@ -1,21 +1,88 @@
 #include "utils.h"
 
 
+// template <typename T>
+// auto part1(const T& input)
+// {
+//     aoc::timer timer;
+//     int result = 0;
+
+//     for (const auto& bank: input)
+//     {
+//         int jolt_max = 0;
+//         auto size = bank.size();
+//         for (auto i = 0U; i < size; ++i)
+//         {
+//             int jolt_i = (bank[i] - '0') * 10;
+//             for (auto j = i + 1; j < size; ++j)
+//             {
+//                 int jolt_j = bank[j] - '0';
+//                 int jolt   = jolt_i + jolt_j;
+//                 if (jolt > jolt_max) jolt_max = jolt;                
+//             }
+//         }
+
+//         result += jolt_max;
+//     }
+
+//     return result;
+// }
+
+
+// Find the (first) highest value battery in the bank starting from 'start' and leaving 
+// at least 'extra' for the remaining smaller digits in our 12-digit result.
+uint64_t find_next(uint64_t input, size_t start, size_t extra, const std::string& bank)
+{
+    if (extra == 0) return input;
+
+    uint64_t jolt_max = 0;
+
+    auto size   = bank.size();
+    auto start2 = start;
+    for (auto i = start; i < (size - extra + 1); ++i)
+    {       
+        uint64_t jolt = bank[i] - '0';
+        if (jolt > jolt_max) 
+        {
+            // Highest value battery since 'start'            
+            jolt_max = jolt;
+            // The next call to find_next starts after this battery.               
+            start2   = i + 1;
+        } 
+    }
+
+    return find_next(input * 10U + jolt_max, start2, extra - 1, bank);
+}
+
+
+// Re-implementation in terms of Part 2 solution.
 template <typename T>
 auto part1(const T& input)
 {
     aoc::timer timer;
-    int result = 0;
+    uint64_t result = 0U;
+
+    for (const auto& bank: input)
+    {
+        auto jolt = find_next(0U, 0U, 2U, bank);
+        result += jolt;
+    }
 
     return result;
 }
 
 
 template <typename T>
-auto part2(T& input)
+auto part2(const T& input)
 {
     aoc::timer timer;
-    int result = 0;
+    uint64_t result = 0U;
+
+    for (const auto& bank: input)
+    {
+        auto jolt = find_next(0U, 0U, 12U, bank);
+        result += jolt;
+    }
 
     return result;
 }
@@ -23,13 +90,7 @@ auto part2(T& input)
 
 void run(const char* filename)
 {
-    //template <class... Args>
-    //std::vector<std::tuple<Args...>> read_lines(std::string filename, std::string pat, const std::string& delim = ";")
-    auto lines = aoc::read_lines<int,int>(filename, R"((\d+)\s+(\d+))");
-
-    // Read all lines from a file into a vector of strings. Trims lines by default and drops empty lines by default. Keeping 
-    // empty lines can be useful when the blank lines in the input are meaningful.
-    //std::vector<std::string> read_lines(std::string filename, Blanks allow_blanks = Blanks::Suppress, Trim trim_lines = Trim::Yes);
+    auto lines = aoc::read_lines(filename);
 
     auto p1 = part1(lines);
     cout << "Part1: " << p1 << '\n';
