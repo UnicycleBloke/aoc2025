5fdd451571d2208c9d92f4439706fa1156132449
Al Chambers <unicycle.bloke@gmail.bloke>
2025-12-05 05:30:55 +0000

    Solved Day 5

--- day05/solution.cpp
+++ day05/solution.cpp
@@ -1,23 +1,64 @@
 #include "utils.h"
 
 
-template <typename T>
-auto part1(const T& input)
+template <typename T, typename U>
+auto part1(const T& ranges, const U& stocks)
 {
     aoc::timer timer;
     int result = 0;
+
+    for (auto [s]: stocks)
+    {
+        for (auto [b, e]: ranges)
+        {
+            if ((s >= b) && (s <= e)) 
+            {
+                ++result;
+                break;
+            }
+        }
+    }
 
     return result;
 }
 
 
 template <typename T>
-auto part2(T& input)
+auto part2(T& ranges)
 {
     aoc::timer timer;
-    int result = 0;
+    uint64_t result = 0;
 
-    return result;
+    // Tuples conventiently sort in just the way we need. We want 
+    // to the order the ranges by start value and then by end value.
+    sort(ranges.begin(), ranges.end());
+
+    uint64_t beg = 0;
+    uint64_t end = 0;
+
+    // Deal with overlapping ranges by extending the end value of a 
+    // combined range until the start value of a range falls after the
+    // end of the combined ranges. Then add the size of the combined range
+    // to the total and start a fresh one.  
+    for (auto [b, e]: ranges)
+    {
+        if (b > end)
+        {
+            result += (end - beg) + 1;
+            beg = b;
+            end = e;
+        }
+        else if (e > end)
+        {
+            end = e;
+        }
+    }
+    result += (end - beg) + 1;
+
+    // Account for the extra value due to the initial null range. Should 
+    // probably account for empty ranges better in general, but this seems 
+    // sufficient for the problem.
+    return result - 1;
 }
 
 
@@ -25,16 +66,13 @@ void run(const char* filename)
 {
     //template <class... Args>
     //std::vector<std::tuple<Args...>> read_lines(std::string filename, std::string pat, const std::string& delim = ";")
-    auto lines = aoc::read_lines<int,int>(filename, R"((\d+)\s+(\d+))");
+    auto ranges = aoc::read_lines<uint64_t, uint64_t>(filename, R"((\d+)-(\d+))");
+    auto stocks = aoc::read_lines<uint64_t>(filename, R"((\d+))");
 
-    // Read all lines from a file into a vector of strings. Trims lines by default and drops empty lines by default. Keeping 
-    // empty lines can be useful when the blank lines in the input are meaningful.
-    //std::vector<std::string> read_lines(std::string filename, Blanks allow_blanks = Blanks::Suppress, Trim trim_lines = Trim::Yes);
-
-    auto p1 = part1(lines);
+    auto p1 = part1(ranges, stocks);
     cout << "Part1: " << p1 << '\n';
 
-    auto p2 = part2(lines);
+    auto p2 = part2(ranges);
     cout << "Part2: " << p2 << '\n';
 }
 
