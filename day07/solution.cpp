157df5c1d0df29a9f8e6dcc21dae19a15aafc2a0
Al Chambers <unicycle.bloke@gmail.bloke>
2025-12-07 05:45:59 +0000

    Solved Day 7

--- day07/solution.cpp
+++ day07/solution.cpp
@@ -5,31 +5,109 @@ template <typename T>
 auto part1(const T& input)
 {
     aoc::timer timer;
-    int result = 0;
 
-    return result;
+    // Use a set for the beams to avoid duplicates after splits.
+    set<uint32_t> beams;
+    uint32_t      splits = 0;
+
+    // Find the initial beam location.
+    const auto& row = input[0]; 
+    for (auto b: aoc::range(row.size()))
+    {
+        if (row[b] == 'S')
+        {
+            beams.insert(b);
+            break;
+        }
+    }
+
+    // For each beam either propogate or split, creating a revised set of beams.
+    for (auto r: aoc::range(1U, input.size()))
+    {
+        const auto& row = input[r]; 
+
+        set<uint32_t> beams2;
+        for (auto b: beams)
+        {
+            if (row[b] == '^')
+            {
+                // Split the beam.
+                beams2.insert(b - 1);
+                beams2.insert(b + 1);
+                ++splits;
+            }
+            else
+            {
+                beams2.insert(b);
+            }
+        }
+
+
+        beams = beams2; 
+    }
+
+    return splits;
 }
 
 
 template <typename T>
-auto part2(T& input)
+auto part2(const T& input)
 {
     aoc::timer timer;
-    int result = 0;
+    
+    // Almost identical logic to Part1 but use a map instead of a set to keep track of the 
+    // numbers of timelines in each column. Sum these at the end for the result.
+    map<uint32_t, uint64_t> beams;
+
+    // Find the initial beam location.
+    const auto& row = input[0]; 
+    for (auto b: aoc::range(row.size()))
+    {
+        if (row[b] == 'S')
+        {
+            beams[b] = 1;
+            break;
+        }
+    }
+
+    // For each beam either propogate or split, creating a revised set of beams.
+    for (auto r: aoc::range(1U, input.size()))
+    {
+        const auto& row = input[r]; 
+
+        map<uint32_t, uint64_t> beams2;
+        for (auto [b, lines]: beams)
+        {
+            if (row[b] == '^')
+            {
+                // Split the beam.
+                beams2[b - 1] = beams2[b - 1] + lines;
+                beams2[b + 1] = beams2[b + 1] + lines;
+            }
+            else
+            {
+                beams2[b] = beams2[b] + lines;
+            }
+        }
+
+
+        beams = beams2; 
+    }
+
+    uint64_t result = 0;
+    for (auto [b, lines]: beams)
+    {
+        result += lines;
+    }
 
     return result;
 }
 
 
+
 void run(const char* filename)
 {
-    //template <class... Args>
-    //std::vector<std::tuple<Args...>> read_lines(std::string filename, std::string pat, const std::string& delim = ";")
-    auto lines = aoc::read_lines<int,int>(filename, R"((\d+)\s+(\d+))");
-
-    // Read all lines from a file into a vector of strings. Trims lines by default and drops empty lines by default. Keeping 
-    // empty lines can be useful when the blank lines in the input are meaningful.
-    //std::vector<std::string> read_lines(std::string filename, Blanks allow_blanks = Blanks::Suppress, Trim trim_lines = Trim::Yes);
+    auto lines = aoc::read_lines(filename);
 
     auto p1 = part1(lines);
     cout << "Part1: " << p1 << '\n';
