7203219d70f63b9d72f495bd556edc4288fe6e40
Al Chambers <unicycle.bloke@gmail.bloke>
2025-12-08 06:35:42 +0000

    Solved Day 8. The code is a bit nasty - could be tidied up.

--- day08/solution.cpp
+++ day08/solution.cpp
@@ -2,12 +2,107 @@
 
 
 template <typename T>
-auto part1(const T& input)
+auto part1(const T& boxes)
 {
     aoc::timer timer;
     int result = 0;
 
-    return result;
+    using Pair = tuple<uint64_t, size_t, size_t>;
+    vector<Pair> pairs;
+
+    for (auto i: aoc::range(boxes.size()))
+    {
+        auto [xi, yi, zi] = boxes[i];
+        for (auto j: aoc::range(i + 1U, boxes.size()))
+        {
+            auto [xj, yj, zj] = boxes[j];
+            auto dx = (xj - xi) * (xj - xi);
+            auto dy = (yj - yi) * (yj - yi);
+            auto dz = (zj - zi) * (zj - zi);
+            auto dist = dx + dy + dz; 
+
+            pairs.push_back(make_tuple(dist, i, j));
+        }
+    }
+
+    using Circuit = set<size_t>;
+    vector<Circuit> circuits;
+
+    sort(pairs.begin(), pairs.end());
+
+    const size_t kConnections = (boxes.size() > 20U) ? 1000U : 10U;
+    for (auto p: aoc::range(pairs.size()))
+    {
+        auto [d, i, j] = pairs[p];
+        
+        size_t ci = -1;
+        size_t cj = -1;
+        for (auto c: aoc::range(circuits.size()))
+        {
+            if (circuits[c].find(i) != circuits[c].end())
+                ci = c;
+            if (circuits[c].find(j) != circuits[c].end())
+                cj = c;
+        }
+
+        if ((ci == -1) && (cj == -1))
+        {
+            Circuit circuit;
+            circuit.insert(i);
+            circuit.insert(j);
+            circuits.push_back(circuit);
+        }
+        else if (ci == -1)
+        {
+            circuits[cj].insert(i);
+
+            if (circuits[cj].size() == boxes.size())
+            {
+                auto [xi, yi, zi] = boxes[i];
+                auto [xj, yj, zj] = boxes[j];
+                cout << xi << " * " << xj << " = " << (xi * xj) << "\n";
+                break;
+            }
+        }
+        else if (cj == -1)
+        {
+            circuits[ci].insert(j);           
+
+            if (circuits[ci].size() == boxes.size())
+            {
+                auto [xi, yi, zi] = boxes[i];
+                auto [xj, yj, zj] = boxes[j];
+                cout << xi << " * " << xj << " = " << (xi * xj) << "\n";
+                break;
+            }
+        }
+        else if (ci != cj)
+        {
+            for (auto x: circuits[ci])
+                circuits[cj].insert(x);
+            circuits[ci].clear();             
+
+            if (circuits[cj].size() == boxes.size())
+            {
+                auto [xi, yi, zi] = boxes[i];
+                auto [xj, yj, zj] = boxes[j];
+                cout << xi << " * " << xj << " = " << (xi * xj) << "\n";
+                break;
+            }
+        }
+
+        if ((p + 1) == kConnections)
+        {
+            vector<size_t> sizes;
+            for (const auto& c: circuits)
+                sizes.push_back(c.size());
+            sort(sizes.begin(), sizes.end(), std::greater<size_t>());
+
+            cout << sizes[0] * sizes[1] * sizes[2] << "\n";
+        }
+    }
+
+    return 0;
 }
 
 
@@ -23,13 +118,7 @@ auto part2(T& input)
 
 void run(const char* filename)
 {
-    //template <class... Args>
-    //std::vector<std::tuple<Args...>> read_lines(std::string filename, std::string pat, const std::string& delim = ";")
-    auto lines = aoc::read_lines<int,int>(filename, R"((\d+)\s+(\d+))");
-
-    // Read all lines from a file into a vector of strings. Trims lines by default and drops empty lines by default. Keeping 
-    // empty lines can be useful when the blank lines in the input are meaningful.
-    //std::vector<std::string> read_lines(std::string filename, Blanks allow_blanks = Blanks::Suppress, Trim trim_lines = Trim::Yes);
+    auto lines = aoc::read_lines<int64_t, int64_t, int64_t>(filename, R"((\d+),(\d+),(\d+))");
 
     auto p1 = part1(lines);
     cout << "Part1: " << p1 << '\n';
